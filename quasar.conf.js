105d52da50b51eb3b0de1af58eb66709b1461801
Vinícius Jardim <viniciuspjardim@gmail.com>
2020-02-27 14:59:27 -0300

    Updating default dev server port

--- quasar.conf.js
+++ quasar.conf.js
@@ -69,7 +69,7 @@ module.exports = function (ctx) {
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       // https: true,
-      // port: 8080,
+      port: 8081,
       open: true // opens browser window automatically
     },
 
